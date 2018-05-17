25f014a0edb77cfe6b3ae55e9c813c9f06589cd8
liuxinxin1205 <1521171265@qq.com>
2018-05-18 07:48:48 +0800

    我的函数库

--- jslib.js
+++ jslib.js
@@ -71,3 +71,36 @@ function clone(obj){
     }
     return newObj;
 }
+
+
+
+
+//cookie
+//添加cookie expires以天为单位
+function addCookie(key,val,expires){
+    var str = key + '=' + val;
+    if(expires != undefined){
+        var date = new Date();
+        date.setDate(date.getDate() + expires);
+        str += ';expires=' + date.toUTCString();
+    }
+    document.cookie = str;
+}
+
+//获取cookie
+function getCookie(key){
+    var arr = document.cookie.split(';');
+    for(var i=0;i<arr.length;i++){
+        var temp = arr[i].split('=');
+        if(temp[0].trim() == key){
+            return temp[1];
+        }
+    }
+}
+
+//删除cookie
+function delCookie(key){
+    var date = new Date();
+    date.setDate(date.getDate()-1);
+    document.cookie = key + '=null' + ';expires = ' + date.toUTCString();
+}
