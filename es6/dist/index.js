71bd16bc35fd249504902f495d2423b05c07d8a8
liuxinxin1205 <1521171265@qq.com>
2018-06-13 09:06:48 +0800

    ES6

---
+++ es6/dist/index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var a = 5;
+var add = function add(x, y) {
+    return x + y;
+};
+
+var arr = [5, 6, 7];
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+    for (var _iterator = arr[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var item = _step.value;
+
+        console.log(item);
+    }
+} catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+} finally {
+    try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+            _iterator.return();
+        }
+    } finally {
+        if (_didIteratorError) {
+            throw _iteratorError;
+        }
+    }
+}
---
+++ es6/src/index.js
@@ -0,0 +1,7 @@
+let a = 5;
+let add = (x,y)=> x+y;
+
+let arr = [5,6,7];
+for(let item of arr){
+    console.log(item); 
+}
